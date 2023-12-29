e113fd2579d327c0adb1af74cae49c77270a710f
Mayuge <mayuge.studio.public@gmail.com>
2023-12-29 20:05:22 +0900

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,24 +4,37 @@ local opt = vim.opt
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- gui
+opt.showmode = false
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+opt.title = true
+opt.foldenable = false
+opt.linebreak = true
+opt.pumblend = 10
+opt.pumheight = 15
+opt.wrap = true
+opt.signcolumn = 'yes'
+opt.list = true
+opt.listchars = { trail = '.', tab = '»-»' }
+opt.smoothscroll = true
+
 -- encoding
 vim.scriptencoding = 'utf-8'
 opt.encoding = 'utf-8'
 opt.fileencoding = 'utf-8'
 
-opt.guicursor = ""
-
--- scroll
-opt.scroll = 5
+-- tabs
+opt.tabstop = 4
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
 
 -- indent
 opt.autoindent = true
 opt.breakindent = true
-
--- タブ, インデント
-opt.tabstop = 4
-opt.expandtab = true
-opt.shiftwidth = 4
 opt.smartindent = true
 
 -- search
@@ -30,19 +43,8 @@ opt.ignorecase = true
 opt.smartcase = true
 opt.incsearch = true
 
--- 補完
-opt.completeopt = 'menuone,noinsert'
-
--- row
-opt.number = true
-opt.relativenumber = true
-opt.cursorline = true
-
 -- view
 opt.ambiwidth = 'double'
-opt.wrap = false
-opt.showmatch = true
-opt.helplang = 'ja', 'en'
 opt.clipboard = 'unnamedplus'
 opt.visualbell = true
 
@@ -51,12 +53,4 @@ opt.winblend = 20
 
 -- updatetime
 opt.updatetime = 300
-
--- signcolumn
-opt.signcolumn = 'yes'
-
--- golang
-vim.api.nvim_exec([[
-  autocmd FileType go setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab
-]], false)
 
