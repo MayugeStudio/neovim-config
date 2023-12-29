6e47edf634f03e09bb574af95290aa00dbab3994
Mayuge <mayuge.studio.public@gmail.com>
2023-12-29 18:26:36 +0900

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,15 +7,8 @@ h.nmap('<leader>sv', '<C-w>v')
 h.nmap('<leader>sh', '<C-w>s')
 h.nmap('<leader>se', '<C-w>=')
 h.nmap('<leader>sx', ':close<CR>')
-h.nmap('<leader>h', '<C-w>h')
-h.nmap('<leader>l', '<C-w>l')
-h.nmap('<leader>j', '<C-w>j')
-h.nmap('<leader>k', '<C-w>k')
 
 -- move
-h.nmap('<leader>n', 'ko<Esc>')
-h.nmap('<leader>m', 'o<Esc>')
-h.nmap('<leader><CR>', 'i<CR><Esc>')
 h.nmap('H', '^')
 h.nmap('L', '$')
 
